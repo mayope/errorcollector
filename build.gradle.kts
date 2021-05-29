6f2d729f47145a0022d0b7ec39e97bbd76538796
Lukas Meisegeier <MeisegeierLukas@gmx.de>
2021-05-29 16:26:21 +0200

    added releasing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("maven-publish")
+    id("signing")
+    id("net.researchgate.release") version ("2.8.0")
 
     // static code analysis
     id("io.gitlab.arturbosch.detekt") version "1.17.1"
@@ -90,6 +93,96 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
 }
 
-tasks.named("build") {
-    dependsOn(gradle.includedBuild("versions").task(":build"))
+tasks.named("afterReleaseBuild") {
+    dependsOn(
+        "publishMavenJavaPublicationToGitHubPackagesRepository"
+    )
+}
+val sourcesJar by tasks.creating(Jar::class) {
+    dependsOn.add(tasks.javadoc)
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn.add(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            groupId = "net.mayope.errorcollector"
+            artifact(sourcesJar)
+            artifact(javadocJar)
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/mayope/errorcollector")
+            credentials {
+                val githubUser = project.findProperty("githubPublishUser") as String? ?: ""
+                username = githubUser
+                val githubAccessToken = project.findProperty("githubPublishKey") as String? ?: ""
+                password = githubAccessToken
+                if (githubUser.isBlank() || githubAccessToken.isBlank()) {
+                    logger.warn("Github user and password are not set you won't be able to publish to github!")
+                }
+            }
+        }
+    }
+}
+
+val publications = project.publishing.publications.withType(MavenPublication::class.java).map {
+    with(it.pom) {
+        withXml {
+            val root = asNode()
+            root.appendNode("name", "errorcollector")
+            root.appendNode("description", "Collects error messages ad sends them to Microsoft Teams or Telegram")
+            root.appendNode("url", "https://github.com/mayope/errorcollector")
+        }
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/mayope/errorcollector")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("klg71")
+                name.set("Lukas Meisegeier")
+                email.set("MeisegeierLukas@gmx.de")
+            }
+        }
+        scm {
+            url.set("https://github.com/mayope/errorcollector")
+            connection.set("scm:git:git://github.com/mayope/errorcollector.git")
+            developerConnection.set("scm:git:ssh://git@github.com/mayope/errorcollector.git")
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+        allprojects {
+            extra["signing.keyId"] = "5357AC31"
+            extra["signing.secretKeyRingFile"] = project.findProperty("signing_key_ring_file")
+            extra["signing.password"] = project.findProperty("signing_key_ring_file_password")
+        }
+    }
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
